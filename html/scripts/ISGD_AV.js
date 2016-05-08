a47872d900750c53967dbf109fe4d4721f697e70
hajmousa <hajmousa@hotmail.com>
2016-05-08 01:03:59 +0000

    Tookout the pin control and added the serial interface

--- html/scripts/ISGD_AV.js
+++ html/scripts/ISGD_AV.js
@@ -1,7 +1,9 @@
 
  webiopi().ready(init);
- 
- 
+ serialusb0 = new Serial("usb0");
+ serialusb1 = new Serial("usb1");
+
+
  function init () {
 
  				var updateLightHours = function(macro, args, response) {
@@ -12,42 +14,37 @@
 				}
 
 				//Configure Port Direction
-				webiopi().setFunction(17,"out");
+				    webiopi().setFunction(17,"out");
         		webiopi().setFunction(18,"out");
         		webiopi().setFunction(22,"out");
         		webiopi().setFunction(23,"out");
-        		
+
 				//Create Handlers
         		var content, button;
         		content = $("#content");
-				
+/*
 				//Create buttons
         		button = webiopi().createGPIOButton(17,"Prayer Hall");
         		content.append(button);
-        		
+
         		button = webiopi().createGPIOButton(18,"Admin");
         		content.append(button);
-        		
+
         		button = webiopi().createGPIOButton(22,"Nursery");
         		content.append(button);
-        		
+
         		button = webiopi().createGPIOButton(23,"GYMNASIUM");
         		content.append(button);
-				
+
 				//Create edit boxes to show on/off
 				webiopi().callMacro("getLightHours", [], updateLightHours);
-				
+
 				button = webiopi().createButton("sendButton", "Send", function() {
 							var hours = [$("#inputOn").val(), $("#inputOff").val()];
 							webiopi().callMacro("setLightHours", hours, updateLightHours);
 						});
-
 				content.append(button);
-
-
-				// Refresh GPIO buttons
+*/				// Refresh GPIO buttons
 				// pass true to refresh repeatedly of false to refresh once
 				webiopi().refreshGPIO(true);
-
-				
 				};
