94cc134c131fb37b1ffb830f77799b5cda44a78f
isgd <hajmousa@gmail.com>
2016-09-30 05:11:02 +0000

    Changes related to reading polycomm and updaing the slider at power up and after updating values.

--- html/scripts/ISGD_AV.js
+++ html/scripts/ISGD_AV.js
@@ -5,53 +5,26 @@
 
 
  function init () {
-
- 				var updateLightHours = function(macro, args, response) {
-							var hours = response.split(";");
-							// Following lines use jQuery functions
-							$("#inputOn").val(hours[0]);
-							$("#inputOff").val(hours[1]);
-				}
-
-				//Configure Port Direction
-				    webiopi().setFunction(17,"out");
-        		webiopi().setFunction(18,"out");
-        		webiopi().setFunction(22,"out");
-        		webiopi().setFunction(23,"out");
-
-				//Create Handlers
-        		var content, button;
-        		content = $("#content");
-/*
-				//Create buttons
-        		button = webiopi().createGPIOButton(17,"Prayer Hall");
-        		content.append(button);
-
-        		button = webiopi().createGPIOButton(18,"Admin");
-        		content.append(button);
-
-        		button = webiopi().createGPIOButton(22,"Nursery");
-        		content.append(button);
-
-        		button = webiopi().createGPIOButton(23,"GYMNASIUM");
-        		content.append(button);
-
-				//Create edit boxes to show on/off
-				webiopi().callMacro("getLightHours", [], updateLightHours);
-
-				button = webiopi().createButton("sendButton", "Send", function() {
-							var hours = [$("#inputOn").val(), $("#inputOff").val()];
-							webiopi().callMacro("setLightHours", hours, updateLightHours);
-						});
-				content.append(button);
-*/				// Refresh GPIO buttons
-				// pass true to refresh repeatedly of false to refresh once
-				webiopi().refreshGPIO(true);
-				};
+//Send command to get different fader values.
+serialusb1.write('get fader "MH_OUTPUT" '+"\r\n");
+serialusb1.write('get fader "FOYER_OUTPUT" '+"\r\n");
+serialusb1.write('get fader "GYM_OUTPUT" '+"\r\n");
+//wait for time delay of 1 seconds to allow polycomm reply
+setTimeout(PolycommInit, 500);
+}
+//Function to parse responses and populate default values.
+        function PolycommInit(){
+          serialusb1.read(function(data){
+            data1 = data;
+            MH_OUTPUT.value = data1.split("MH_OUTPUT").pop().substring(1,7).replace(/[^\d.-]/g, '');
+            data1 = data;
+            FOYER_OUTPUT.value = data1.split("FOYER_OUTPUT").pop().substring(1,7).replace(/[^\d.-]/g, '');
+            data1 = data;
+            GYM_OUTPUT.value = data1.split("GYM_OUTPUT").pop().substring(1,7).replace(/[^\d.-]/g, '');;
+        });
+      }
         //Jquery events
         $(document).ready(function(){
-
-
             $('.isgdmute').on("vclick", function() {
                   DisableButton(this);
             });
@@ -63,49 +36,46 @@
                 ATEN_Comamnd(this);
             });
         });
-        //Mute actions
-        // Video controls
+        function AtenCollectData(){
+          serialusb0.read(function(data) {
+            document.getElementById("Aten_Response").value = data;
+          });
+        }
+        function PolycommCollectData(who){
+          serialusb1.read(function(data){
+            //read response and make extract volume control.
+            temp = data.split(who.id).pop().substring(1);
+            document.getElementById("Polycomm_Response").value = temp
+            who.value = temp.trim();
+          });
+        }
+
         function ATEN_Comamnd(who) {
         	if (who.value == 0)
         		document.getElementById("Aten_Command").value="sw o0"+who.name+" off";
         	else
         		document.getElementById("Aten_Command").value="sw i0"+who.value +" o0"+who.name;
-        	//Send Data to serial port
+        	//Send Data o serial port
             serialusb0.write(document.getElementById("Aten_Command").value+"\r\n");
-
-            var data;
-            serialusb0.read(function(data) {
-              alert("received " + data)
-            });
-            serialusb0.read(function(data) {
-              alert("received " + data)
-            });
-            document.getElementById("Aten_Response").value = data;
+            setTimeout(AtenCollectData, 500);
         }
         //Polycomm Audio controls.
         function ChangeAudioVolume(who)
         {
+          //flush buffer
           if(who.id == "MH_OUTPUT"){
-              if(  document.getElementById("MH_MUTE").checked ==false)
-                serialusb1.write('set fader "MH_OUTPUT" '+ who.value+"\r\n");
-            }
+            if(  document.getElementById("MH_MUTE").checked ==false)
+              serialusb1.write('set fader "MH_OUTPUT" '+ who.value+"\r\n");
+          }
           else if(who.id == "FOYER_OUTPUT"){
-              if(  document.getElementById("FY_MUTE").checked ==false)
-                  serialusb1.write('set fader "FOYER_OUTPUT" '+ who.value+"\r\n");
-                }
+            if(  document.getElementById("FY_MUTE").checked ==false)
+              serialusb1.write('set fader "FOYER_OUTPUT" '+ who.value+"\r\n");
+          }
           else if(who.id == "GYM_OUTPUT"){
-              if(  document.getElementById("GY_MUTE").checked ==false)
-                  serialusb1.write('set fader "GYM_OUTPUT" '+who.value+"\r\n");
-                }
-           var data
-           serialusb1.read(function(data) {
-            alert("received " + data)
-          });
-          serialusb1.read(function(data) {
-           alert("received " + data)
-         });
-          document.getElementById("Polycomm_Response").value = data;
-
+            if(  document.getElementById("GY_MUTE").checked ==false)
+              serialusb1.write('set fader "GYM_OUTPUT" '+who.value+"\r\n");
+          }
+          setTimeout(PolycommCollectData, 500,who);
         }
 
         function DisableButton(who)
@@ -115,15 +85,12 @@
               if(who.id == "MH_MUTE"){
                 serialusb1.write('set mute "MH_OUTPUT" 1'+"\r\n");
                 serialusb1.write('set mute "Main_Prayer_Hall" 1'+"\r\n");
-                //$('#MH_OUTPUT').slider('disable');
-                //document.getElementById("MH_OUTPUT").disabled = true;
+                $("#MH_OUTPUT").hide();
               }else if (who.id =="FY_MUTE"){
                 serialusb1.write('set mute "FOYER_OUTPUT" 1'+"\r\n");
-                //document.getElementById("FOYER_OUTPUT").disabled = true;
               }else if (who.id =="GY_MUTE"){
                 serialusb1.write('set mute "GYM_OUTPUT" 1'+"\r\n");
                 serialusb1.write('set mute "Gym_Mic" 1'+"\r\n");
-                  //document.getElementById("GYM_OUTPUT").disabled = true;
               }
             }
             else
@@ -131,8 +98,7 @@
               if(who.id == "MH_MUTE"){
                 serialusb1.write('set mute "MH_OUTPUT" 0'+"\r\n");
                 serialusb1.write('set mute "Main_Prayer_Hall" 0'+"\r\n");
-                //$('#MH_OUTPUT').slider('enable');
-                //document.getElementById("MH_OUTPUT").disabled = false;
+                $("#MH_OUTPUT").show();
               }else if (who.id =="FY_MUTE"){
                 serialusb1.write('set mute "FOYER_OUTPUT" 0'+"\r\n");
                 //document.getElementById("FOYER_OUTPUT").disabled = false;
